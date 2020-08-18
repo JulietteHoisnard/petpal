65529cf43940173178cf16659a86cbe6a16c963f
Juliette Hoisnard <66121617+JulietteHoisnard@users.noreply.github.com>
2020-08-19 00:22:13 +0200

    Book a pet (#7)
    * controller and routes Appointments done
    
    * controller user
    
    * appointment work in progress
    
    * appointment new
    
    * added flatpickr and date select in show
    
    * fix

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,28 @@
+class AppointmentsController < ApplicationController
+  def new
+    @pet = Pet.find(params[:pet_id])
+    # @pet = Pet.find(1)
+    @appointment = Appointment.new
+  end
+
+  def create
+    @pet = Pet.find(params[:pet_id])
+    # @pet = Pet.find(1)
+    @appointment.pet = @pet
+    if @appointment.save
+      puts "appointment saved sucessfully!"
+      # redirect_to dashboard
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def appointment_params
+    params
+    .require(:appointment)
+    .permit(:pet_id, :start_date_time, :end_date_time)
+  end
+  
+end
