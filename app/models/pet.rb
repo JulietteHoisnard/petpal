2279ca96bfb1ba5eb02eb2cb0f35d3dbe3d27bed
Juliette Hoisnard <juliette@hoisnard.fr>
2020-08-19 12:36:56 +0200

    add cloudinary and .env

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,4 @@
 class Pet < ApplicationRecord
   has_many :appointments
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_214627) do
+ActiveRecord::Schema.define(version: 2020_08_19_095356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,8 @@ ActiveRecord::Schema.define(version: 2020_08_18_214627) do
     t.string "location"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_pets_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -75,4 +77,5 @@ ActiveRecord::Schema.define(version: 2020_08_18_214627) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "appointments", "pets"
   add_foreign_key "appointments", "users"
+  add_foreign_key "pets", "users"
 end
