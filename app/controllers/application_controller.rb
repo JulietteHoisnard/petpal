9d1005556e8dc8b465f965e31265d26a4d21cad9
Juliette Hoisnard <juliette@hoisnard.fr>
2020-08-20 16:33:12 +0200

    fixed authentification issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,21 @@
 class ApplicationController < ActionController::Base
+  # before_action :store_location
   before_action :authenticate_user!
+  # before_action :store_user_location!, if: :storable_location?
   
   # def after_sign_in_path_for(resource)
   #   if session[:form_data].present?
-  #     pet_path(@pet)
+  #     dashboard_path
   #   end
   # end
+
+  def after_sign_in_path_for(resource)
+    if session[:pet_id]
+      pet_path(session[:pet_id])      
+    else
+      root_path
+    end
+  end
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
